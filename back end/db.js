ac2c6484048354c3bf8a713a1b0ed1b329fdd1c8
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-02-24 20:40:53 -0500

    Delete back end directory

--- back end/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://SDEV255:Password255@coursedb.tntth.mongodb.net/?retryWrites=true&w=majority&appName=CourseDB", {useNewUrlParser: true})
-
-module.exports = mongoose
